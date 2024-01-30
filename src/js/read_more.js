8241a72265b2f3728fdbc35ffbc1200dd11159f5
Irulik <iryna_n@hotmail.com>
2024-01-30 13:52:19 +0100

    added avatars.js, modal.js, read_more.js, updated teachers.html, added popup_login.css, read_more.css, apdated teachers.css

---
+++ src/js/read_more.js
@@ -0,0 +1,23 @@
+console.log('Hello world!');
+
+document.querySelectorAll('.read-more-btn').forEach(button => {
+    button.addEventListener('click', () => {
+        const dataIndex = button.parentElement.dataset.index; 
+        const cardId = `card${dataIndex}`;
+        const parentContainer = document.querySelector(`[data-index="${dataIndex}"]`);
+        const additionalInfo = parentContainer.querySelector('.tutor-readmore');
+        const bookLessonButton = parentContainer.querySelector('.book-button');
+
+        if (additionalInfo) {
+            additionalInfo.classList.toggle('tutor-readmore--show');
+        }
+
+        if (button.classList.contains('read-more-btn')) {
+            button.classList.toggle('is-hidden');
+        }
+
+        if (bookLessonButton) {
+            bookLessonButton.classList.toggle('is-hidden');
+        }
+    });
+});
