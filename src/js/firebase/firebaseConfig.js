d6bceb6ed891074d4c4228fca236392641f1b249
Irulik <iryna_n@hotmail.com>
2024-02-02 10:45:09 +0100

    added adaptive to header.css, home.css, added file .env.example, updated firebaseConfig.js

--- src/js/firebase/firebaseConfig.js
+++ src/js/firebase/firebaseConfig.js
@@ -1,5 +1,7 @@
+const SECRET_KEY = import.meta.env.VITE_SECRET_KEY;
+
 export const firebaseConfig = {
-  apiKey: "AIzaSyBnbF93L_e-YM1l0IfpVG6xe1yQypttTqs",
+  apiKey: SECRET_KEY,
   authDomain: "base-lingo.firebaseapp.com",
   datafaseURL: 'https://base-lingo-default-rtdb.europe-west1.firebasedatabase.app',
   projectId: "base-lingo",
