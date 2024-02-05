c00ad38283accc795cb245fe8b90036cda0d9d5b
Irulik <iryna_n@hotmail.com>
2024-02-05 17:49:03 +0100

    updated firebaseConfig.js

--- src/js/firebase/firebaseConfig.js
+++ src/js/firebase/firebaseConfig.js
@@ -1,7 +1,7 @@
-const SECRET_KEY = import.meta.env.VITE_SECRET_KEY;
+const VITE_SECRET_KEY = import.meta.env.VITE_SECRET_KEY;
 
 export const firebaseConfig = {
-  apiKey: SECRET_KEY,
+  apiKey: VITE_SECRET_KEY,
   authDomain: "base-lingo.firebaseapp.com",
   datafaseURL: 'https://base-lingo-default-rtdb.europe-west1.firebasedatabase.app',
   projectId: "base-lingo",
